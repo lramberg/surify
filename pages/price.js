8f9ed07bbe8ddea5637ee07ddf8114167c24fb1f
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2021-04-08 21:12:25 -0700

    style price

--- pages/price.js
+++ pages/price.js
@@ -1,26 +1,28 @@
-import { TextField, FormControlLabel, Checkbox, Paper, Typography } from "@material-ui/core";
+import {
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
 import axios from "axios";
-import Button from '../components/Button/Button'
-import { Grid } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-
-
+import Button from "../components/Button/Button";
+import { Grid } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const useStyles = makeStyles({
   container: {
-    backgroundColor: '#F0F3F9',
-    minHeight: '100vh',
-    position: 'relative',
-    
+    backgroundColor: "#F0F3F9",
+    minHeight: "100vh",
+    position: "relative",
+    padding: 64,
   },
-  
+
   leftSide: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
 
   form: {
@@ -28,20 +30,53 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "column",
     maxWidth: 700,
-    margin: '2.5%',
-    marginLeft: '25%',
+    maxHeight: 700,
     fontFamily: "Menlo",
-    borderRadius: '16px'
-
+    borderRadius: "16px",
   },
 
   logo: {
-    position: 'absolute',
-    width: '94.5px',
-    height: '45px',
-    left: '64px',
-    top: '64px',
-  }
+    width: "94.5px",
+    height: "45px",
+    marginBottom: 130,
+  },
+
+  title: {
+    fontSize: 18,
+    lineHeight: "20px",
+    margin: 0,
+    marginBottom: 24,
+  },
+
+  subtitle: {
+    fontSize: 14,
+    lineHeight: "20px",
+    color: "#515E76",
+    margin: 0,
+    marginBottom: 64,
+  },
+
+  otherTitle: {
+    fontSize: 12,
+    lineHeight: "16px",
+    fontWeight: 600,
+    color: "#515E76",
+    margin: 0,
+    marginBottom: 8,
+  },
+
+  otherSubtitle: {
+    fontSize: 14,
+    lineHeight: "20px",
+    color: "#0F2042",
+    margin: 0,
+  },
+
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: 27,
+  },
 });
 
 function PricePage() {
@@ -52,57 +87,55 @@ function PricePage() {
   const [price, setPrice] = useState(0);
 
   async function getPrice() {
-    const brokerSheetId = window.localStorage.getItem('brokerSheetId')
-    
+    const brokerSheetId = window.localStorage.getItem("brokerSheetId");
+
     const data = {
-      'broker_sheet_id': brokerSheetId,
-      'traveler': {
-        "is_high_risk_area": isHighRisk,
-        "total_days_traveling": daysTraveling,
-        "is_luxury_stay": isLuxury
-      }
-    }
-    
+      broker_sheet_id: brokerSheetId,
+      traveler: {
+        is_high_risk_area: isHighRisk,
+        total_days_traveling: daysTraveling,
+        is_luxury_stay: isLuxury,
+      },
+    };
+
     try {
-        await axios.post('https://tdwbtw.herokuapp.com/api/premiums/calculate/', data).then(function(response) {
-          console.log("getPrice >  successs ", response)
-          setPrice(response.data.price)
-        })
+      await axios
+        .post("https://tdwbtw.herokuapp.com/api/premiums/calculate/", data)
+        .then(function (response) {
+          console.log("getPrice >  successs ", response);
+          setPrice(response.data.price);
+        });
     } catch (error) {
-      console.log('getPrice > error ', error)
+      console.log("getPrice > error ", error);
     }
   }
 
-  
   return (
     <Grid container className={classes.container}>
       <CssBaseline />
 
       <Grid item md={4} className={classes.leftSide}>
-        <Box 
-          className={classes.logo}
-          component='img'
-          src='/eerie_insurance.png'
-          height={40}
-        />
+        <img className={classes.logo} src="/eerie_insurance.png" />
+        <div>
+          <p className={classes.title}>CHUBB</p>
 
-        <Typography variant="h6">
-          CHUBB
-        </Typography>
+          <p className={classes.subtitle}>
+            Fill out the following form to receive your free Passport 360 quote
+          </p>
 
-        <Typography variant="body1">
-          Fill out the following form to receive your free Passport 360 quote
-          <br></br>
-          PREPARED BY
-          <br></br>
-          Jon Snow at Erie insurance
-          <br></br>
-          AGENCY CONTACT
-          <br></br>
-          +1 323-222-0101
-        </Typography> 
+          <div className={classes.wrapper}>
+            <p className={classes.otherTitle}>PREPARED BY</p>
+
+            <p className={classes.otherSubtitle}>Jon Snow at Erie insurance</p>
+          </div>
+          <div className={classes.wrapper}>
+            <p className={classes.otherTitle}>AGENCY CONTACT</p>
+
+            <p className={classes.otherSubtitle}>+1 323-222-0101</p>
+          </div>
+        </div>
       </Grid>
-      
+
       <Grid md={8} item component={Paper} className={classes.form}>
         <h1>Here's Your Price!</h1>
 
@@ -133,10 +166,8 @@ function PricePage() {
         />
 
         <h2>Total Due Today: ${price}</h2>
-        <Button text={'Submit'} onClick={getPrice} />
+        <Button text={"Submit"} onClick={getPrice} />
       </Grid>
-      
-
     </Grid>
   );
 }
