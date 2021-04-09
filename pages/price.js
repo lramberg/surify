6486bb2a67e5af43317f1e5cb9f053adba8ce28e
Ken Mendonça <ken.mendonca@sureapp.com>
2021-04-08 18:46:12 -0700

    wip

--- pages/price.js
+++ pages/price.js
@@ -5,12 +5,22 @@ import axios from "axios";
 import Button from '../components/Button/Button'
 import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
 
 
 const useStyles = makeStyles({
   container: {
     backgroundColor: '#F0F3F9',
-    minHeight: '100vh'
+    minHeight: '100vh',
+    position: 'relative',
+    
+  },
+  
+  leftSide: {
+    display: 'flex',
+    flexDirection: 'column',
   },
 
   form: {
@@ -18,8 +28,8 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "column",
     maxWidth: 700,
-    margin: '5%',
-    border: '1px solid black',
+    margin: '2.5%',
+    marginLeft: '25%',
     fontFamily: "Menlo",
     borderRadius: '16px'
 
@@ -66,7 +76,9 @@ function PricePage() {
   
   return (
     <Grid container className={classes.container}>
-      <Grid item md={3}>
+      <CssBaseline />
+
+      <Grid item md={4} className={classes.leftSide}>
         <Box 
           className={classes.logo}
           component='img'
@@ -78,18 +90,20 @@ function PricePage() {
           CHUBB
         </Typography>
 
-        <p>Fill out the following form to receive your free Passport 360 quote</p>
-          <p>
-            PREPARED BY <br></br>
-            Jon Snow at Erie insurance
-            <br></br>
-            <br></br>
-            AGENCY CONTACT <br></br>
-            +1 323-222-0101
-          </p>    
+        <Typography variant="body1">
+          Fill out the following form to receive your free Passport 360 quote
+          <br></br>
+          PREPARED BY
+          <br></br>
+          Jon Snow at Erie insurance
+          <br></br>
+          AGENCY CONTACT
+          <br></br>
+          +1 323-222-0101
+        </Typography> 
       </Grid>
       
-      <Grid md={9} item component={Paper} className={classes.form}>
+      <Grid md={8} item component={Paper} className={classes.form}>
         <h1>Here's Your Price!</h1>
 
         <FormControlLabel
@@ -118,8 +132,8 @@ function PricePage() {
           onChange={(e) => setDaysTraveling(e.target.value)}
         />
 
+        <h2>Total Due Today: ${price}</h2>
         <Button text={'Submit'} onClick={getPrice} />
-      <h2>Total Due Today: ${price}</h2>
       </Grid>
       
 
